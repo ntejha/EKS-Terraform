61acf3362c8642d42c3407b46e6078ec044997cd
Tejha <113423720+ntejha@users.noreply.github.com>
2025-01-05 16:45:08 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.gitops_learn.id
+  value = aws_eks_cluster.gitops.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.gitops_learn.id
+  value = aws_eks_node_group.gitops.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.gitops_learn_vpc.id
+  value = aws_vpc.gitops_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.gitops_learn_subnet[*].id
+  value = aws_subnet.gitops_subnet[*].id
 }
